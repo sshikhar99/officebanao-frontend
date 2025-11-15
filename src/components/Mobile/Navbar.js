da977674e69b4ae9ac9750a7c3116b56768cd775
sshikhar99 <sshikhar494@gmail.com>
2025-11-15 17:29:30 +0530

    ..

--- src/components/Mobile/Navbar.js
+++ src/components/Mobile/Navbar.js
@@ -70,7 +70,7 @@ export default function MobileNavbar() {
         alignItems: "center",
         padding: "12px 20px",
         background: "#111",
-        color: "#facc15",
+        color: "#f8b400",
         position: "sticky",
         top: 0,
         zIndex: 1000,
