10279e9ef807b725b5ccf63d013ec0c1522a874a
sshikhar99 <sshikhar494@gmail.com>
2025-11-24 14:37:18 +0530

    ..

--- src/components/ProjectShowcase.js
+++ src/components/ProjectShowcase.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./ProjectShowcase.css";
-import project1 from "../assets/project1.jpg";
-import project2 from "../assets/project2.jpg";
-import project3 from "../assets/project3.jpg";
+import project1 from "../assets/Projects/project1.jpg";
+import project2 from "../assets/Projects/project2.jpg";
+import project3 from "../assets/projects/project3.jpg";
 
 const projects = [
   { id: 1, img: project1, title: "Modern Office Interior" },
